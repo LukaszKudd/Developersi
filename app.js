542637e3fb2881a489632b68d007348636868e60
LukaszKudd <lukasz.kuderewski@gmail.com>
2019-12-22 17:32:49 +0200

    Add sample questions

--- app.js
+++ app.js
@@ -4,5 +4,31 @@ const app = express();
 
 app.listen(3000, () => {
     console.log('Server is listening at http://localhost:3000/');
-    
 });
+
+let goodAnswers = 0;
+let callToAFriendUsed = false;
+let questionToTheCrowdUsed = false;
+let halfOnHalfUsed = false;
+
+const questions = [
+    {
+        question: 'W tym pytaniu odpowiedzią poprawną jest A',
+        answers: ['A', 'B', 'C', 'D'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'W tym pytaniu odpowiedzią poprawną jest C',
+        answers: ['A', 'B', 'C', 'D'],
+        correctAnswer: 2,
+    },
+    {
+        question: 'W tym pytaniu odpowiedzią poprawną jest D',
+        answers: ['A', 'B', 'C', 'D'],
+        correctAnswer: 3,
+    },
+];
+
+app.get('/question', (req, res) => {
+    
+})
